6b1461057df2e845665097f41adaca13dbdc91d2
Roman Feoktistov <roman.php@upiple.com>
2024-08-25 16:01:23 -0700

    feature tests added

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\SubmissionController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/submit', [SubmissionController::class, 'submit']);
--- tests/Feature/SubmissionTest.php
+++ tests/Feature/SubmissionTest.php
@@ -10,13 +10,37 @@ class SubmissionTest extends TestCase
 {
     public function test_submission_endpoint()
     {
-        $response = $this->postJson('/api/submit', [
+        $response = $this->postJson('/submit', [
             'name' => 'John Doe',
             'email' => 'john.doe@example.com',
             'message' => 'This is a test message.'
         ]);
 
+        $response->assertJsonPath('message', 'Submission is being processed');
         $response->assertStatus(202);
     }
 
+    public function test_submission_endpoint_with_missing_fields()
+    {
+        $response = $this->postJson('/submit', [
+            'name' => 'John Doe'
+        ]);
+
+        $response->assertJsonPath('message', 'The email field is required. (and 1 more error)');
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['email', 'message']);
+    }
+
+    public function test_submission_endpoint_with_invalid_email()
+    {
+        $response = $this->postJson('/submit', [
+            'name' => 'John Doe',
+            'email' => 'invalid-email',
+            'message' => 'This is a test message.'
+        ]);
+
+        $response->assertJsonPath('message', 'The email field must be a valid email address.');
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['email']);
+    }
 }
